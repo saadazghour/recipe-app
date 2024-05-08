dfb91ab9b4e2c573123c1fe6b286303e8950dd70
Saad AZGHOUR <saad.azghour@gmail.com>
2024-05-08 10:00:57 +0100

    Update Routers

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AddRecipe from '../views/AddRecipe.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/add-recipe',
+    name: 'AddRecipe',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import('../views/AboutView.vue'),
+    component: AddRecipe
+  }
+]
+
+const appRouter = createRouter({
+  // history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
+  routes
 })
 
-export default router
+export default appRouter
