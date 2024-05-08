36eb299bb22f816e212a410bb846c73c3d3a41f6
Saad AZGHOUR <saad.azghour@gmail.com>
2024-05-08 09:33:08 +0100

    fix use of use(appRouter)

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import '../src/assets/main.css'
+
 import App from './App.vue'
-import router from './router'
+import appRouter from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(appRouter)
 
 app.mount('#app')
